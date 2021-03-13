43722ee3cbfb612722e09f97edf33e8a6215b086
mukraj91 <mukraj91@gmail.com>
2021-03-13 18:21:08 +0530

    Added some new array concepts

--- Array.cpp
+++ Array.cpp
@@ -84,6 +84,119 @@ cout<<"=========================================================="<<endl;
 cout<<"*******************************Google Mission - 24-July-20202****************************"<<endl;
 
 
+
+------------------------------------------------------
+
+Pointer to Array:- Pointer to an array is also known as Array pointer.
+We are using the pointer to access the components of the array.
+-------------------
+
+Data-type (*variable-name)[size of array];
+
+int (*ptr)[10];
+
+int (*ptr)[10] = NULL; // Pointer to array of ten numbers
+
+Ex :-
+
+int (*ptr)[5];   // Pointer to interger of five numbers
+
+int fun[5] = {1, 2, 3, 4, 5}
+
+
+ptr = &array  // Point to whole array of fun
+
+int i = 0;
+
+for (i = 0 ; i < 5 ; i++) {
+
+cout << "Element of the array : " << *(*ptr + i) <<endl;
+
+}
+================================
+Array of Pointer:- is an array of the pointer variable .
+It is also know as pointer array.
+
+
+Data-type *variable[size of array];
+
+int *ptr[1o];
+
+
+
+Ex:-
+
+
+int ptr[5] = {1, 2, 3, 4, 5};
+
+int *fun[5];
+
+int i;
+
+
+for (i = 0 ; i < 5 ; i++) {
+
+fun[i] = &ptr[i];
+
+}
+
+
+// Printing value using Pointer
+
+
+for (int i = 0 ; i < ptr ; i++) {
+
+cout << "Print the value of array : " << *(*ptr +i) <<::std::endl;
+
+}
+
+=====================================
+// Array of pointer
+
+char *name[] = {
+                "Mukesh",
+                "RojarFast",
+                "Hanuman"
+}
+
+for (int i = 0 ; i < 3 ; i++) {
+
+cout << "Elements are :" << *(name+i) <<::std::endl;
+
+}
+
+==========================
+
+int (*Pointer-to-array)[5];
+
+int *array-point-to-zero-location;
+int array-point-to-whole-location[5];
+
+
+array-point-to-zero-location = Pointer-to-array; // Points to 0th element of the arr.
+    p = arr;
+
+array-point-to-whole-location = &Pointer-to-array; // Points to the whole array arr.
+
+
+========================
+int arr[3][4] = {
+                    {10, 11, 12, 13},
+                    {20, 21, 22, 23},
+                    {30, 31, 32, 33}
+                  };
+  int (*ptr)[4];
+  ptr = arr;
+  printf("%p %p %p\n", ptr, ptr + 1, ptr + 2);
+  printf("%p %p %p\n", *ptr, *(ptr + 1), *(ptr + 2));
+  printf("%d %d %d\n", **ptr, *(*(ptr + 1) + 2), *(*(ptr + 2) + 3));
+  printf("%d %d %d\n", ptr[0][0], ptr[1][2], ptr[2][3]);
+
+===============================================================================================
+***********************************************************************************************
+===============================================================================================
+
+
 return 0;
 
 }
