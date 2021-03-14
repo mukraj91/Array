c0ea89c8f95e1cf7555b56aad22743fbb3c06737
mukraj91 <mukraj91@gmail.com>
2021-03-14 12:39:30 +0530

    Compile Array code

--- Array.cpp
+++ Array.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include<vector>
+#include<stdio.h>
 
 using namespace std;
 
 int main()
 
 {
+
+/*
+
 
 cout<<"*******************************Google Mission - 23-July-20202****************************"<<endl;
 
@@ -26,6 +30,7 @@ cout<<"*******************************Google Mission - 23-July-20202************
      cout <<"Address of ["<< i <<"] is: " << &google[i]<<"\n";
    }
 
+
    cout<<"=========================================================="<<endl;
 
   // void  alexa[]; // error: declaration of ‘alexa’ as array of void
@@ -35,7 +40,7 @@ cout<<"*******************************Google Mission - 23-July-20202************
 
    int youtube[] = {100, 200, 300, 400}; // int (*)[4]
 
-   //int *android = &youtube; // error: cannot convert ‘int (*)[4]’ to ‘int*’ in initialization
+    int *android = &youtube; // error: cannot convert ‘int (*)[4]’ to ‘int*’ in initialization
 
    cout<<"Address of &youtube : " << &youtube <<endl;
 
@@ -46,10 +51,12 @@ cout<<"*******************************Google Mission - 23-July-20202************
 
 
 cout<<"=========================================================="<<endl;
+*/
 
 
 // Array member access using pointer arithmetics.
 
+/*
 
 int googlepay[] = {1 , 2 , 3 , 4 , 5 , 6};
 
@@ -82,89 +89,104 @@ for (auto value = googlevector.begin(); value != googlevector.end() ; value++)
 
 cout<<"=========================================================="<<endl;
 cout<<"*******************************Google Mission - 24-July-20202****************************"<<endl;
+*/
 
 
 
-------------------------------------------------------
 
+/*------------------------------------------------------
 Pointer to Array:- Pointer to an array is also known as Array pointer.
+
 We are using the pointer to access the components of the array.
 -------------------
 
-Data-type (*variable-name)[size of array];
+Data-type (*variable-name)[size of array];*/
 
-int (*ptr)[10];
+/*int (*ptr)[10];
 
-int (*ptr)[10] = NULL; // Pointer to array of ten numbers
+int (*ptr1)[10] = NULL; // Pointer to array of ten numbers
 
-Ex :-
+//Ex :-
 
-int (*ptr)[5];   // Pointer to interger of five numbers
+int (*ptr2)[5];   // Pointer to interger of five numbers
 
-int fun[5] = {1, 2, 3, 4, 5}
+int fun[5] = {1, 2, 3, 4, 5};
 
 
-ptr = &array  // Point to whole array of fun
+ptr2 = &fun;  // Point to whole array of fun
 
 int i = 0;
 
 for (i = 0 ; i < 5 ; i++) {
 
-cout << "Element of the array : " << *(*ptr + i) <<endl;
+cout << "Element of the array : " << *(*ptr2 + i) <<endl;
 
-}
+}*/
+
+
+
+/*
 ================================
+
 Array of Pointer:- is an array of the pointer variable .
 It is also know as pointer array.
 
-
 Data-type *variable[size of array];
 
-int *ptr[1o];
+
+*/
 
 
 
-Ex:-
+/*int *ptr3[10];
 
 
-int ptr[5] = {1, 2, 3, 4, 5};
+int ptr5[5] = {1, 2, 3, 4, 5};
 
-int *fun[5];
+int *fun[5]; // Pointer array
 
 int i;
 
 
 for (i = 0 ; i < 5 ; i++) {
 
-fun[i] = &ptr[i];
+fun[i] = &ptr5[i];
 
 }
-
 
 // Printing value using Pointer
 
 
-for (int i = 0 ; i < ptr ; i++) {
+for (int i = 0 ; i < 5 ; i++) {
 
-cout << "Print the value of array : " << *(*ptr +i) <<::std::endl;
+cout << "Print the value of array : "<< *(ptr5 +i)<<::std::endl;
 
-}
+}*/
 
+
+
+
+/*
 =====================================
-// Array of pointer
+ Array of pointer
+*/
+
+/*int i;
 
 char *name[] = {
                 "Mukesh",
                 "RojarFast",
                 "Hanuman"
-}
+};
 
 for (int i = 0 ; i < 3 ; i++) {
 
 cout << "Elements are :" << *(name+i) <<::std::endl;
 
-}
+}*/
 
+
+/*
 ==========================
 
 int (*Pointer-to-array)[5];
@@ -178,23 +200,34 @@ array-point-to-zero-location = Pointer-to-array; // Points to 0th element of the
 
 array-point-to-whole-location = &Pointer-to-array; // Points to the whole array arr.
 
+*/
 
-========================
+
 int arr[3][4] = {
                     {10, 11, 12, 13},
                     {20, 21, 22, 23},
                     {30, 31, 32, 33}
                   };
-  int (*ptr)[4];
-  ptr = arr;
-  printf("%p %p %p\n", ptr, ptr + 1, ptr + 2);
-  printf("%p %p %p\n", *ptr, *(ptr + 1), *(ptr + 2));
-  printf("%d %d %d\n", **ptr, *(*(ptr + 1) + 2), *(*(ptr + 2) + 3));
-  printf("%d %d %d\n", ptr[0][0], ptr[1][2], ptr[2][3]);
 
-===============================================================================================
+  int (*ptr)[4]; // Pointer to array which is called array Pointer
+
+  ptr = arr;
+
+  cout<< "ptr = "<< ptr << " ptr + 1 = "<< ptr + 1 << " ptr + 2 = " << ptr + 2 << "ptr +3 = " <<ptr +3 <<std::endl;
+
+   printf("%p %p %p\n", *ptr, *(ptr + 1), *(ptr + 2));
+
+   printf("%d %d %d\n", **ptr, *(*(ptr + 1) + 2), *(*(ptr + 2) + 3));
+
+   printf("%d %d %d\n", ptr[0][0], ptr[1][2], ptr[2][3]);
+
+
+/*===============================================================================================
 ***********************************************************************************************
 ===============================================================================================
+
+*/
+
 
 
 return 0;
